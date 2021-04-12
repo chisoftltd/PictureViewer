0f3f50cf4d9e921973f355900d47a4e34194a484
chisoftltd <chisoftltd@gmail.com>
2021-04-12 21:06:51 +0200

    Add project files.

---
+++ PictureViewer/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace PictureViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLoadImages = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.openFD1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(160, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(720, 750);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(720, 750);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLoadImages
+            // 
+            this.btnLoadImages.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLoadImages.Location = new System.Drawing.Point(13, 10);
+            this.btnLoadImages.Name = "btnLoadImages";
+            this.btnLoadImages.Size = new System.Drawing.Size(126, 39);
+            this.btnLoadImages.TabIndex = 1;
+            this.btnLoadImages.Text = "Load Images";
+            this.btnLoadImages.UseVisualStyleBackColor = true;
+            this.btnLoadImages.Click += new System.EventHandler(this.btnLoadImages_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Images";
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 99);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(130, 650);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // openFD1
+            // 
+            this.openFD1.FileName = "openFileDialog1";
+            this.openFD1.Multiselect = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(64, 64);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 761);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLoadImages);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Picture Viewer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLoadImages;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.OpenFileDialog openFD1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ PictureViewer/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureViewer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public DialogResult ofdResults { get; private set; }
+        public int numOfFiles { get; private set; }
+        public string[] aryFilePaths { get; private set; }
+        public int counter { get; private set; }
+
+        private void btnLoadImages_Click(object sender, EventArgs e)
+        {
+            imageList1.Images.Clear();
+            listView1.Clear();
+
+            openFD1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            openFD1.Title = "Open Image Files";
+            openFD1.Filter = "JPEGS|*.jpg|GIFS|*.gif|PNG|*.png";
+
+            ofdResults = openFD1.ShowDialog();
+            if (ofdResults==DialogResult.Cancel)
+            {
+                return;
+            }
+
+            try
+            {
+                numOfFiles = openFD1.FileName.Length;
+                aryFilePaths = new string[numOfFiles];
+                counter = 0;
+
+                foreach (string singleFile in openFD1.FileNames)
+                {
+                    aryFilePaths[counter] = singleFile;
+                    imageList1.Images.Add(Image.FromFile(singleFile));
+                    listView1.LargeImageList = imageList1;
+                    counter++;
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+    }
+}
